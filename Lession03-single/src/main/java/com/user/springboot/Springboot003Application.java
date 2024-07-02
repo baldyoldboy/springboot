9c583599e771142a4562d8710f6c9e1bd488b5ac
辉 袁 <1403687669@qq.com>
2024-07-02 21:11:52 +0800

    one

---
+++ Lession03-single/src/main/java/com/user/springboot/Springboot003Application.java
@@ -0,0 +1,13 @@
+package com.user.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lession03singleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lession03singleApplication.class, args);
+    }
+
+}
