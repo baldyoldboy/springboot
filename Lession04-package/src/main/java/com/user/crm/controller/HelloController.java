f09e37a7449503b7299d3e4cb8e9740e9cc0f22b
辉 袁 <1403687669@qq.com>
2024-07-03 11:10:34 +0800

    user2做了修改

--- Lession04-package/src/main/java/com/user/crm/controller/HelloController.java
+++ Lession04-package/src/main/java/com/user/crm/controller/HelloController.java
@@ -15,6 +15,8 @@ public class HelloController {
     public String hello(){
         System.out.println("==================");
 
+        System.out.println("=======第二次修改===========");
+        System.out.println("sdfdsfdsfsdf");
         return "欢迎来到SpringBoot";
 
 
